b6528587913e45f6015a1ab1368602b9febd36e4
unknown <quangtruong01234@gmail.com>
2023-10-31 17:58:25 +0700

    clear deploy

--- src/pages/teller/listOrder/ListOrder.tsx
+++ src/pages/teller/listOrder/ListOrder.tsx
@@ -1,4 +1,4 @@
-import SearchFilter from '@/components/SearchFilter'
+// import SearchFilter from '@/components/SearchFilter'
 import Pagination from '@/components/Pagination'
 import { useEffect, useState } from 'react'
 import TableOrder from '@/components/teller/TableOrder'
