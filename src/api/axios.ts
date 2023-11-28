6d1459fe603ec8f4c57c65a99c186d81315181c5
Tấn Trung <nguyentantrung1801@gmail.com>
2023-11-28 11:56:01 +0700

    update noti

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// export const BASE_URL = 'http://localhost:3500/'
+//export const BASE_URL = 'https://localhost:7235/api'
 export const BASE_URL = 'https://thanh-huy.azurewebsites.net/api'
 export default axios.create({
     baseURL: BASE_URL
