ed7e1f7deb0d82db8026c8f31208c6733435f6b5
Tấn Trung <nguyentantrung1801@gmail.com>
2023-12-05 20:33:08 +0700

    clear code

--- src/services/revenueService.ts
+++ src/services/revenueService.ts
@@ -5,14 +5,14 @@ export class Private {
         const axiosPrivate = userAxiosPrivate();
         const queryParams = new URLSearchParams({...filters , pageNumber: pageNumber.toString()});
         //URLSearchParams không dùng encodeURIComponent
-        console.log('query revenue', queryParams.toString());
+        //console.log('query revenue', queryParams.toString());
         return await axiosPrivate.get(`/revenues?${queryParams}`)
     }
 
     getStatics = async (filters:any) => {
         const axiosPrivate = userAxiosPrivate();
         const queryParams = new URLSearchParams({...filters});
-        console.log('query statics', queryParams.toString());
+        //console.log('query statics', queryParams.toString());
         return await axiosPrivate.get(`/revenues/statics?${queryParams}`)
     }
 }
